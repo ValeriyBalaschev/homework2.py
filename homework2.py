d35939f330628b2e6324be4db9f0b21fe6b4bf93
ValeriyBalaschev <bwengs.ru@mail.ru>
2024-06-30 14:08:42 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+# homework2.py Практическое задание по теме "Переменные.
+homework_completed = 12
+hours_spent = 1.5
+course_name = 'Pithon'
+nime_per_task = hours_spent / homework_completed
+print("Курс:", course_name, ', всего задач:', homework_completed, ', затрачено часов: ', hours_spent,
+      'среднее время выполнения ', nime_per_task, 'часа')
